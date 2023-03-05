56a81fb8c388a9c6ff2d3e951cc0f75f62fc18b5
fe-soares <felipe1998s@gmail.com>
2023-03-05 15:58:32 -0300

    validação de formulario para o login de usuario, não esta chamando serviço ainda apenas formulario

--- WebApp/Barca/src/app/nav/authentication/authentication.module.ts
+++ WebApp/Barca/src/app/nav/authentication/authentication.module.ts
@@ -5,6 +5,7 @@ import { LoginComponent } from "./login/login.component";
 import { RegisterComponent } from "./register/register.component";
 import { AuthenticationAppComponent } from "./authentication.app.component";
 import { SharedModule } from "../shared/shared.module";
+import { ReactiveFormsModule } from "@angular/forms";
 
 @NgModule({
     declarations: [
@@ -13,6 +14,7 @@ import { SharedModule } from "../shared/shared.module";
         AuthenticationAppComponent
     ],
     imports: [
+        ReactiveFormsModule,
         CommonModule,
         AuthenticationRoutingModule,
         SharedModule
--- WebApp/Barca/src/app/nav/authentication/login/login.component.ts
+++ WebApp/Barca/src/app/nav/authentication/login/login.component.ts
@@ -1,4 +1,9 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChildren } from '@angular/core';
+import { FormControlName, FormBuilder, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { fromEvent, merge, Observable } from 'rxjs';
+import { ValidationMessages, DisplayMessage, GenericValidator } from '../../shared/generic-forms';
+import { User } from '../models/User';
 
 @Component({
   selector: 'app-login',
@@ -6,5 +11,55 @@ import { Component } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent {
+  @ViewChildren(FormControlName, {read: ElementRef}) formInputElements: ElementRef[] = [];
 
+  validationMessages :ValidationMessages = {};
+  displayMessage: DisplayMessage = {};
+  genericValidator: GenericValidator;
+
+  mudancasNaoSalvas: boolean = false;
+  isNewUser: boolean = false;
+
+  registerFormRender: boolean = false;
+
+  user: User = {} as User;
+  constructor(private fb: FormBuilder, private router : Router){
+      this.validationMessages = {
+          Email:{
+              required: 'O email é requerido',
+              email: 'Precisa ser um email valido'
+          },
+          Password: {
+              required: 'Informe o a senha',
+          },
+      };
+
+      this.genericValidator = new GenericValidator(this.validationMessages);
+  }
+
+  loginForm = this.fb.group({
+      Email: ['', [Validators.required, Validators.email]],
+      Password: ['', Validators.required]
+  });
+
+  private generate(formGroup: any): void{
+    let controlBlurs: Observable<any>[] = this.formInputElements.map((formControl: ElementRef) => 
+    fromEvent(formControl.nativeElement, 'blur'));
+
+    merge(...controlBlurs).subscribe(() => {
+        this.displayMessage = this.genericValidator.processarMensagens(formGroup);
+        this.mudancasNaoSalvas = true;
+    });
+  }
+
+  ngAfterViewInit(): void {
+      this.generate(this.loginForm);
+  }
+
+  login(){
+    if(this.loginForm.valid){
+        Object.assign(this.user, this.loginForm.value);
+        this.router.navigate(['home']);
+    }
+  }
 }
